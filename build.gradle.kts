11eadb01dda75b714cf28c064d55e98c31472d57
forpleuvoir <695801070@qq.com>
2024-12-20 00:49:29 +0800

    更新版本号及依赖
    - 修改 `mod_version` 为 `3.0.0a-dev`，调整版本格式拼接规则。
    - 更新 `fabricloader` 依赖最低版本为 `0.16.0`，`fabric` 依赖最低版本为 `0.112.0`。
    - 升级 `ibukigourd` 至 `0.7.2d-dev.ac87196`，调整相关依赖版本定义。

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ val gitHash: String by lazy {
 }
 
 val modName: String = properties["archives_base_name"].toString()
-version = properties["mod_version"].toString() + "+$gitHash"
+version = properties["mod_version"].toString() + ".$gitHash"
 group = properties["maven_group"].toString()
 
 dependencies {

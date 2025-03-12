c4f747af2dabe07b7cae90332c30c3ad0ff4dab0
forpleuvoir <695801070@qq.com>
2025-03-13 00:57:51 +0800

    修改`HitResultExtensions.kt`中的`hitBlock`扩展属性逻辑，添加了类型和空值判断以避免类型转换异常。

--- src/client/kotlin/moe/forpleuvoir/hiirosakura/util/HitResultExtensions.kt
+++ src/client/kotlin/moe/forpleuvoir/hiirosakura/util/HitResultExtensions.kt
@@ -2,7 +2,13 @@ package moe.forpleuvoir.hiirosakura.util
 
 import net.minecraft.client.MinecraftClient
 import net.minecraft.util.hit.BlockHitResult
-
+import net.minecraft.util.hit.HitResult
 
 val MinecraftClient.hitBlock: BlockHitResult?
-    get() = crosshairTarget as BlockHitResult?
+    get() {
+        val target = crosshairTarget
+        if (target != null && target.type == HitResult.Type.BLOCK) {
+            return target as BlockHitResult
+        }
+        return null
+    }
